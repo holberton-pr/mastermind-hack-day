537b36898c8e1109dfa16cdfd070d94e40e8b4f7
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-16 03:12:28 +0000

    Pushing changes

--- mastermind.java
+++ mastermind.java
@@ -9,7 +9,7 @@
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
-
+import java.lang.String;
 
 class mastermind {
 
@@ -17,8 +17,10 @@ class mastermind {
 
                 // Generates a random number between 1000 and 9999
                 int random_num = ThreadLocalRandom.current().nextInt(1000, 10000);
+                //declares variable used for collecting user input
                 Scanner usr_input = new Scanner (System.in);
-                int usr_number;
+                int usr_number = 0;
+                int num_of_tries = 1;
 
                 System.out.println("Welcome to Mastermind!");
                 System.out.println("For questions about to play the game please visit: ");
@@ -29,12 +31,29 @@ class mastermind {
 
                 System.out.println("Please enter a 4 digit number:");
                 usr_number = usr_input.nextInt();
-                System.out.println(usr_number);
 
                 if (random_num == usr_number) {
                   System.out.println("You're a mastermind! You got the right number on your first try!");
                 }
 
+                while (random_num != usr_number)
+                {
+                  System.out.println("Please enter a 4 digit number:");
+                  usr_number = usr_input.nextInt();
+                  num_of_tries++;
+
+                  if (random_num == usr_number) {
+                    System.out.println("Congrats! You've found the secret code. You are a mastermind.");
+                    System.out.println("It took you " + num_of_tries + " tries!");
+                  }
+
+                  String number = new String(String.valueOf(usr_number));
+                  System.out.println(number.getClass().getName());
+                  System.out.println(number.substring(0, 1));
+                  System.out.println(number.substring(1, 2));
+                  System.out.println(number.substring(2, 3));
+                  System.out.println(number.substring(3, 4));
+                }
 
       }
 }
